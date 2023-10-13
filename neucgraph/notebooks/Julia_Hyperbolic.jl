9caa0dc758c53f1f2a4120c08d12f1aa09606393
Jakub 'lambdaofgod' Bartczuk <bartczukkuba@gmail.com>
2023-10-14 00:40:27 +0200

    julia updates

--- neucgraph/notebooks/Julia_Hyperbolic.jl
+++ neucgraph/notebooks/Julia_Hyperbolic.jl
@@ -17,6 +17,7 @@ begin
 	using LightGraphs # graph library
 	using SimpleWeightedGraphs
 	using StatisticalGraphics
+	ENV["LD_LIBRARY_PATH"] = ""
 end
 
 # ╔═╡ 461df8e8-cb6d-4927-8f31-48b591fbc194
@@ -25,14 +26,20 @@ begin
 	path = "../data/org_roam_records_2023_10_08.json"
 end
 
+# ╔═╡ 4115223d-d5ca-43df-98f8-a30aac00ed94
+using ManifoldLearning
+
 # ╔═╡ 3cb49c9e-793a-4bc7-a6f6-6fddc72f0ae4
 begin
 	using Manifolds
 	using Manopt
 	using Random
 	using Plots
-	Random.seed!(42);
+	Random.seed!(42)
 end
+
+# ╔═╡ 3a7f0c50-a31e-41d0-a687-72957dafeec6
+
 
 # ╔═╡ b1c737a4-ba9c-4335-b2c3-73d72a79e79e
 records = JSON.parse(read(path, String));
@@ -178,6 +185,59 @@ ds_poincare = Dataset(x=X[:,1], y=X[:,2], name=get_sorted_vertices(labeled_graph
 # ╔═╡ 61391d79-07f9-489d-918b-65551e8fc9f8
 sgplot(ds_poincare, StatisticalGraphics.Scatter(x=:x,y=:y,labelresponse=:name))
 
+# ╔═╡ fa277357-69b9-4abe-a6fe-1346d7034a54
+md"""
+## Regular MDS
+"""
+
+# ╔═╡ 351af441-c417-43bb-974b-ba7a296c2b6a
+begin
+	mds = fit(MDS, dists * 1.0; distances=true, maxoutdim=2);
+	Y_mds = predict(mds);
+	ds_mds = Dataset(x=Y_mds[1,:], y=-Y_mds[2,:], name=get_sorted_vertices(labeled_graph));
+	sgplot(ds_mds, StatisticalGraphics.Scatter(x=:x,y=:y,labelresponse=:name))
+end
+
+# ╔═╡ 83626734-c2b2-478b-aabd-0ef3604786b6
+md"""
+## Isomap
+"""
+
+# ╔═╡ ee66593f-93ab-4f92-9316-2e9361d9fa85
+begin
+	isomap = fit(Isomap, dists * 1.0);
+	Y_isomap = predict(mds);
+	ds_isomap = Dataset(x=Y_isomap[1,:], y=-Y_isomap[2,:], name=get_sorted_vertices(labeled_graph));
+	sgplot(ds_isomap, StatisticalGraphics.Scatter(x=:x,y=:y,labelresponse=:name))
+end
+
+# ╔═╡ 9781e526-71b1-4e4d-a0e7-fe127104fbd9
+
+
+# ╔═╡ 5baa86c4-1972-4779-9f54-8f0cc63b892d
+
+
+# ╔═╡ 4527c79a-4657-4331-a9a0-3068021aff15
+sgplot(ds_mds, StatisticalGraphics.Scatter(x=:x,y=:y,labelresponse=:name))
+
+# ╔═╡ 0bd00c5a-00df-45f4-a2ad-5710fd85b40f
+
+
+# ╔═╡ d2a79196-c747-438d-b6bf-047074dd99e9
+
+
+# ╔═╡ 44d958ea-b0af-448a-8eef-c926ec2862a3
+
+
+# ╔═╡ c31c7488-a8e0-4dbb-a43f-167645b44c8d
+
+
+# ╔═╡ 7f082478-f761-4e1e-ab6c-66f0d643b760
+
+
+# ╔═╡ ea18299b-e2ff-47ab-a4e4-6881a05e6be4
+
+
 # ╔═╡ 5875994b-39da-4861-9e2c-5aaf12033a21
 md"""
 ## Manifolds
@@ -188,17 +248,26 @@ md"""
 # ╔═╡ bea8e470-8266-42c5-b399-38735fbff300
 lorenz_model = Hyperbolic(2)
 
-# ╔═╡ ef77dc32-1718-419e-8be1-362b1cd55e1b
+# ╔═╡ 30631dbc-c39f-4620-babe-24cdad1bebbe
 begin
 	pts =  [ [0.85*cos(φ), 0.85*sin(φ), sqrt(0.85^2+1)] for φ ∈ range(0,2π,length=11) ]
 	scene = Plots.plot(lorenz_model, pts; geodesic_interpolation=100)
 end
 
-# ╔═╡ 1207f0c4-3f9d-4c3f-a44b-42df584dce3a
-rand(lorentz_manifold)
+# ╔═╡ ef77dc32-1718-419e-8be1-362b1cd55e1b
+
+
+# ╔═╡ 73d77fe2-d921-46c4-b760-60960b77f4d9
+
 
 # ╔═╡ 3210cccf-4295-4382-b9c6-95e6d984c1dd
 p=[0,0,1.0]
+
+# ╔═╡ 3182dd25-262e-45f2-93c6-a201bd857833
+# ╠═╡ disabled = true
+#=╠═╡
+data = [exp(lorentz_manifold, p,  0.01 * rand(lorentz_manifold)) for i in 1:length(X)];
+  ╠═╡ =#
 
 # ╔═╡ af50c3e3-a598-4241-805f-7f2a47e9ec38
 
@@ -212,23 +281,18 @@ p=[0,0,1.0]
 # ╔═╡ bd65f2c5-e0d2-4c18-8226-25a3a06ae099
 
 
+# ╔═╡ 0e89d3c5-463c-4329-b11c-8586cc834fd5
+data = [exp(M, p,  σ * rand(M; vector_at=p)) for i in 1:n];
+
 # ╔═╡ 5078689e-81fc-4710-be33-9f17af00dd48
 data
 
 # ╔═╡ 616f27ef-f0f7-4628-bca7-eeb087707044
 
 
-# ╔═╡ 3182dd25-262e-45f2-93c6-a201bd857833
-# ╠═╡ disabled = true
-#=╠═╡
-data = [exp(lorentz_manifold, p,  0.01 * rand(lorentz_manifold)) for i in 1:length(X)];
-  ╠═╡ =#
-
-# ╔═╡ 0e89d3c5-463c-4329-b11c-8586cc834fd5
-data = [exp(M, p,  σ * rand(M; vector_at=p)) for i in 1:n];
-
 # ╔═╡ Cell order:
 # ╠═5a980518-667b-11ee-1701-8b9b21975813
+# ╠═3a7f0c50-a31e-41d0-a687-72957dafeec6
 # ╠═461df8e8-cb6d-4927-8f31-48b591fbc194
 # ╠═b1c737a4-ba9c-4335-b2c3-73d72a79e79e
 # ╠═c24341de-1bbc-4d2b-bae8-c19ebbc891af
@@ -248,11 +312,26 @@ data = [exp(M, p,  σ * rand(M; vector_at=p)) for i in 1:n];
 # ╠═2b55695f-d53c-4b78-8093-6c8fa0610073
 # ╠═c68d85dd-4961-4cc9-8912-821eb1ac8b35
 # ╠═61391d79-07f9-489d-918b-65551e8fc9f8
+# ╠═fa277357-69b9-4abe-a6fe-1346d7034a54
+# ╠═351af441-c417-43bb-974b-ba7a296c2b6a
+# ╠═83626734-c2b2-478b-aabd-0ef3604786b6
+# ╠═4115223d-d5ca-43df-98f8-a30aac00ed94
+# ╠═ee66593f-93ab-4f92-9316-2e9361d9fa85
+# ╠═9781e526-71b1-4e4d-a0e7-fe127104fbd9
+# ╠═5baa86c4-1972-4779-9f54-8f0cc63b892d
+# ╠═4527c79a-4657-4331-a9a0-3068021aff15
+# ╠═0bd00c5a-00df-45f4-a2ad-5710fd85b40f
+# ╠═d2a79196-c747-438d-b6bf-047074dd99e9
+# ╠═44d958ea-b0af-448a-8eef-c926ec2862a3
+# ╠═c31c7488-a8e0-4dbb-a43f-167645b44c8d
+# ╠═7f082478-f761-4e1e-ab6c-66f0d643b760
+# ╠═ea18299b-e2ff-47ab-a4e4-6881a05e6be4
 # ╠═5875994b-39da-4861-9e2c-5aaf12033a21
 # ╠═3cb49c9e-793a-4bc7-a6f6-6fddc72f0ae4
 # ╠═bea8e470-8266-42c5-b399-38735fbff300
+# ╠═30631dbc-c39f-4620-babe-24cdad1bebbe
 # ╠═ef77dc32-1718-419e-8be1-362b1cd55e1b
-# ╠═1207f0c4-3f9d-4c3f-a44b-42df584dce3a
+# ╠═73d77fe2-d921-46c4-b760-60960b77f4d9
 # ╠═3210cccf-4295-4382-b9c6-95e6d984c1dd
 # ╠═3182dd25-262e-45f2-93c6-a201bd857833
 # ╠═af50c3e3-a598-4241-805f-7f2a47e9ec38
