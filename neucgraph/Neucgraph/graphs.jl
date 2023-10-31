af2a35d71db51875604bd9298029d465e7a6868f
Jakub Bartczuk <bartczukkuba@gmail.com>
2023-10-31 12:47:41 +0100

    embedding distances

--- neucgraph/Neucgraph/graphs.jl
+++ neucgraph/Neucgraph/graphs.jl
@@ -2,6 +2,7 @@ using Distributions
 using Manopt
 using Manifolds
 using LightGraphs
+using ManifoldDiff
 
 ## MODULE
 
@@ -30,12 +31,37 @@ function sample_neighbor_indices(neighbors, vertices, n)
     (anchors, neighbor_indices)
 end
 
-sample_neighbor_indices(positive_indices, [1, 2], 2)
-
-X = rand(M, 4)
+## Manifold and initial embeddings
 M = Hyperbolic(2)
+X = rand(M, named_graph.v_size)
 
+(positive_anchor_indices, sampled_positive_indices) = sample_neighbor_indices(positive_indices, [1, 2], 2)
+(negative_anchor_indices, sampled_negative_indices) = sample_neighbor_indices(negative_indices, [1, 2], 2)
+
+X_anchor = X[positive_anchor_indices]
+X_positive = X[sampled_positive_indices]
+
+function get_manifold_distance(M, X)
+    n = trunc(Int, size(X)[1] / 2)
+    X_anchor = X[1:n]
+    X_positive = X[n+1:end]
+    sum(distance.(Ref(M), X_anchor, X_positive))
+end
+
+## Vectorizing distance computation
+X_mat = hcat(X...)'
+
+X_sample_mat = X_mat[vcat(positive_anchor_indices, sampled_positive_indices),:]
+X_sample = X[vcat(positive_anchor_indices, sampled_positive_indices)]
+
+distance(Ref(M), X_sample_mat[1:4, :], X_sample_mat[5:end,:])
+
+ManifoldDiff.gradient(M, get_manifold_distance, X_sample)
 
 function sample_positive_negative_indices(anchor_indices)
 
 end
+
+
+p0 = [1.0 0.0; 0.0 1.0; 0.0 0.0; 0.0 0.0; 0.0 0.0]
+size(p0)
